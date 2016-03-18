d44e397909818970072b91e3f2b9c58fc92b8167
Anna Becker <anna.r.becker@gmail.com>
2016-03-18 08:28:55 -0700

    Create method to find user from entered username

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,10 @@
 var apiKey = require('./../.env').apiKey;
-
+var getRepos = require('./../js/repos-interface.js').getRepos;
 
 $(document).ready(function() {
-
+ $('#find-git').submit(function(event) {
+   event.preventDefault();
+   var username = $('#username').val();
+   getRepos();
+  });
 });
--- js/repos-interface.js
+++ js/repos-interface.js
@@ -1,7 +1,10 @@
+var apiKey = require('./../.env').apiKey;
+
 exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  var username = $('#username').val();
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
-};
+}
