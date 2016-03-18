aaa8b0a3e4675f44937771e0eeebd36184382c8b
Anna Becker <anna.r.becker@gmail.com>
2016-03-18 08:42:33 -0700

    Format search results.

--- js/repos-interface.js
+++ js/repos-interface.js
@@ -4,6 +4,13 @@ exports.getRepos = function(){
   var username = $('#username').val();
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('.git-results').html('<h1>' + username + '</h1>');
+    if(response.name != null) {
+      $('.git-results').append('<strong><p>Name: </strong>' + response.name + '</p>');
+    };
+    if(response.bio != null) {
+      $('.git-results').append('<storng>Bio:</strong><p>' + response.bio + '</p>');
+    };
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
