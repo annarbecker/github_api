cd6a2b683fb3c21215de0d99c614bf6a1ca73981
Anna Becker <anna.r.becker@gmail.com>
2016-03-18 10:46:17 -0700

    Add user avatar, email, name, and followers to getRepos method.

--- js/git-interface.js
+++ js/git-interface.js
@@ -4,13 +4,13 @@ exports.getUsername = function(){
   var username = $('#username').val();
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $('.username-results').append('<h1>' + username + '</h1><hr>');
+    $('.username-results').append('<h1>' + username + '</h1>');
+    $('.username-results').append("<img src='" + response.avatar_url + "'><br>");
     if(response.name != null) {
-      $('.username-results').append('<strong><p>Name: </strong>' + response.name + '</p><hr>');
+      $('.username-results').append('<strong><p>Name: </strong>' + response.name + '</p>');
     };
-    if(response.bio != null) {
-      $('.username-results').append('<strong><p>Bio: </strong>' + response.bio + '</p><hr>');
-    };
+    $('.username-results').append("<a href='" + response.email + "'>Email " + username + "</a>");
+    $('.username-results').append('<strong><p>Followers: </strong>' + response.followers + '</p>');
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
@@ -20,9 +20,10 @@ exports.getRepos = function(){
   var username = $('#username').val();
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(repos){
     console.log(repos);
-    $('.repo-results2').append("<tr><td>Repository</td><td>Description</td><td>Date Created</td></tr>");
+    console.log(repos.length);
+    $('.repo-results').append("<tr><th>Repository</th><th>Description</th><th>Date Created</th></tr>");
     for(var i = 0; i < repos.length ; i++) {
-      $('.repo-results2').append("<tr><td><a href='https://github.com/" + username + "/" + repos[i].name + "'>" + repos[i].name + "</a></td><td>" + repos[i].description + "</td><td>" + moment(repos[i].created_at).format('L') + "</td></tr>");
+      $('.repo-results').append("<tr><td><a href='https://github.com/" + username + "/" + repos[i].name + "'>" + repos[i].name + "</a></td><td>" + repos[i].description + "</td><td>" + moment(repos[i].created_at).format('L') + "</td></tr>");
     };
   }).fail(function(error){
     console.log(error.responseJSON.message);
